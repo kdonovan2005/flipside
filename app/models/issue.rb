9f89684e1510e84e69d0937a9f61c5ac17bc6546
xristocodos <xristojobos@gmail.com>
2016-06-06 14:43:17 -0400

    postgres changes

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,11 +1,2 @@
 class Issue < ActiveRecord::Base
-  
-  def self.open_issues
-    open_issues = Issue.where("open = ?", true)
-  end
-
-  def self.public_issues
-    public_issues = Issue.where("private = ?", false)
-  end
-
 end
