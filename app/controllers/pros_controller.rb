eee2c2f03d44665c20ab8827d9e8129543e267b8
kdonovan2005 <kdonovan2005@gmail.com>
2016-06-06 16:44:33 -0400

    can add a new pro

--- app/controllers/pros_controller.rb
+++ app/controllers/pros_controller.rb
@@ -5,8 +5,15 @@ class ProsController < ApplicationController
   end
 
   def create
-    @pro = Pro.create
+    binding.pry
+    @pro = Pro.create(pro_params)
     redirect_to issues_path
+  end
+
+  private
+
+  def pro_params
+    params.require(:pro).permit(:name, :weight, :issue_id)
   end
 
 end
