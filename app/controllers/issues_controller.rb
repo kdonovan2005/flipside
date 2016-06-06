13a485f9bf2546aedd6934bc2e688aff7b4036bd
kdonovan2005 <kdonovan2005@gmail.com>
2016-06-06 14:19:14 -0400

    adding views

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -5,4 +5,34 @@ class IssuesController < ApplicationController
     @issues = Issue.open_issues && Issue.public_issues
   end
 
+  def new
+    @issue = Issue.new
+  end
+
+  def create
+    @issue = Issue.create(issue_params)
+    redirect_to issue_path(@issue)
+  end
+
+  def update
+    @issue.update(issue_params)
+    @issue.save
+    redirect_to issue_path(@issue)
+  end
+
+  def destroy
+    @issue.destroy
+    redirect_to issues_path
+  end
+
+  private
+
+  def issue_params
+    params.require(:issue).permit(:name, :open, :private)
+  end
+
+  def select_issue
+    @issue = Issue.find(params[:id])
+  end
+
 end
