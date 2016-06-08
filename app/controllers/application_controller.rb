4da7475a8524b64db4f24f360fa6cbec03d6f0e6
kdonovan2005 <kdonovan2005@gmail.com>
2016-06-08 13:41:12 -0400

    fixed application controller session issue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def current_user
-    session = session[:user_id]
-    @current_user ||= User.find(session) if session
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
   helper_method :current_user
 
