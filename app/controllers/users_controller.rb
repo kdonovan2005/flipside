766c24c375cde0aa110808821a1d4bc126efda30
kdonovan2005 <kdonovan2005@gmail.com>
2016-06-08 15:12:58 -0400

    added error messages to sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,12 +11,12 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.new(user_params)
-    if user.save
-      session[:user_id] = user.id
-      redirect_to '/'
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to root_path
     else
-      redirect_to '/signup'
+      render new_user_path
     end
   end
 
