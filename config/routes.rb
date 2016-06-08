1d4bee2ced7c83f03c1282d253b247bf6193fad8
kdonovan2005 <kdonovan2005@gmail.com>
2016-06-08 10:53:42 -0400

    ajax the delete pro is working

--- config/routes.rb
+++ config/routes.rb
@@ -9,10 +9,10 @@ Rails.application.routes.draw do
   post '/users', to: 'users#create'
 
 
-  resources :issues, only: [:index, :create, :new, :destroy, :show, :update]
+  resources :issues, only: [:index, :create, :new, :destroy, :show, :update, :edit]
   resources :pros, only: [:new, :create, :destroy]
   resources :cons, only: [:new, :create, :destroy]
-  resources :users, only: [:index, :new, :create, :edit, :update, :destroy, :shower]
+  resources :users, only: [:index, :new, :create, :edit, :update, :destroy, :show]
 
   get '/users/:id/delete', to: 'users#confirmdestroy', as: 'confirmdestroy_user'
 end
