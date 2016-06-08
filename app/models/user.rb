ae1c1e5ff90bfe5e6ace77938f3175f4fc73b010
xristocodos <xristojobos@gmail.com>
2016-06-08 12:18:00 -0400

    writing the other user controller crud tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ActiveRecord::Base
   has_many :issues
   has_secure_password
   validates :name, presence: true
-  validates :email, presence: true, uniqueness: true, allow_nil: true
+  validates :email, presence: true, uniqueness: true
 end
--- spec/controllers/user_controller_spec.rb
+++ spec/controllers/user_controller_spec.rb
@@ -14,6 +14,8 @@ describe UsersController, type: :controller do
   let(:missing_email) { attributes.except(:email)}
   let(:missing_pw) { attributes.except(:password)}
 
+
+  ##CREATION##
   it "cannot be created without a name" do
     expect(User.new(missing_name)).not_to be_valid
   end
@@ -25,4 +27,32 @@ describe UsersController, type: :controller do
   it "cannot be created without a password" do
     expect(User.new(missing_pw)).not_to be_valid
   end
+
+
+
+  ##EDIT CREATED FICTIONAL USER##
+  it "cannot find an existing user for updating" do
+    expect(User.edit()).not_to be_valid
+  end
+
+  ##UPDATE CREATED FICTIONAL USER##
+  it "cannot be updated without a name" do
+    expect(User.new(missing_name)).not_to be_valid
+  end
+
+  it "cannot be updated without an email" do
+    expect(User.update(missing_email)).not_to be_valid
+  end
+
+  it "cannot be updated with non-matching pw & confirmation pw" do
+    expect(User.new(missing_email)).not_to be_valid
+  end
+
+
+  ##DESTROY FICTIONAL CREATED USER##
+  it "cannot delete a user that is not the current logged in user" do
+
+  end
+
+
 end
