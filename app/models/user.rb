70607e9f256145509c2b97032be44ef136a09582
kdonovan2005 <kdonovan2005@gmail.com>
2016-06-08 14:32:09 -0400

    added pw length requirement, changed create user view

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ActiveRecord::Base
   has_secure_password
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
+  validates :password, length: {minimum: 6}, on: :create
 end
