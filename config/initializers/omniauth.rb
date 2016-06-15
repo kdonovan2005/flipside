de191867dfd41943259c7c6d360bad332c5a97c2
kdonovan2005 <kdonovan2005@gmail.com>
2016-06-14 20:49:06 -0400

    authenticated?

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, ['TWITTER_KEY'], ['TWITTER_SECRET']
+  provider :twitter, ['ydmsNy5hRau1w4qEYAcI86Ket'], ['YHCrB4kfYMC9gCbU6bWlIEQhNk9RJr2GgCAz5qyjiFCCTfeAfV']
 end
